d0fe2f1aff12d20070fed72ac39c20cd3afb0924
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-02-27 00:33:10 +0300

    The number entered by the user is decreased by 5 until it becomes 0 or negative, and when it reaches the target value, 5 is added until it returns to its previous value.

---
+++ src/HomeworkProjects/StopTargetWithRecursive.java
@@ -0,0 +1,47 @@
+package HomeworkProjects;
+
+import java.util.Scanner;
+
+public class StopTargetWithRecursive {
+
+    // Recursive Technical
+    public static void numberProcess(int number) {
+
+        if (number > 0) {
+            System.out.print(number + " ");
+            numberProcess(number - 5);
+        } else if (number < 0) {
+            System.out.print(number + " ");
+            numberProcess(number + 5);
+        } else {
+            System.out.print(number + " ");
+            for (int i = 5; i <= 20; i += 5) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter The Number: ");
+        int number = input.nextInt();
+        int temp = number;
+
+        numberProcess(number);
+
+        // Classic Technical
+        /*
+        while (temp>0){
+            System.out.print(temp+" " );
+            temp -= 5;
+        }
+        while (temp<=number){
+            System.out.print(temp + " ");
+            temp +=5;
+
+        }
+
+         */
+    }
+}
