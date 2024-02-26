8b0c09652c5d08812dc5fe362c6e5d5034488c8e
FERHAT <73959099+ferhatseker180@users.noreply.github.com>
2024-02-26 15:46:33 +0300

    Fibonacci series calculation with recursive method

--- src/HomeworkProjects/FibonacciSeries.java
+++ src/HomeworkProjects/FibonacciSeries.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class FibonacciSeries {
     public static void main(String[] args) {
 
+        // Import Scanner and Take User Input
         Scanner input = new Scanner(System.in);
         System.out.print("Which element in the Fibonacci sequence do you want to see the result of: ");
         int count = input.nextInt();
 
+        // Show Result
         System.out.println("Result = " + findFibonacci(count));
-
     }
 
+    // Our calculate function
     public static int findFibonacci(int count){
-
         if (count == 1 || count == 2){
             return 1;
         }
-
         return findFibonacci(count - 1) + findFibonacci(count - 2);
     }
 }
